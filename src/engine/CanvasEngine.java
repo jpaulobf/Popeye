b4c3abd580aa19c87b6da8da8215569cf8388629
jpaulobf <jpaulobf@gmail.com>
2022-08-16 15:41:04 -0300

    New interface location

---
+++ src/engine/CanvasEngine.java
@@ -0,0 +1,17 @@
+package engine;
+/**
+ * Game interface (update & draw methods)
+ */
+public interface CanvasEngine {
+    /**
+     * Update the game logic / receives the frametime
+     * @param frametime
+     */
+    public void update(long frametime);
+
+    /**
+     * Draw the game / receives the frametime
+     * @param frametime
+     */
+    public void draw(long frametime);
+}
