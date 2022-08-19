1c7127730f75563b7ad991714d9d9120155cee94
jpaulobf <jpaulobf@gmail.com>
2022-08-19 12:54:00 -0300

    Menu (done)

--- src/game/MenuScreen.java
+++ src/game/MenuScreen.java
@@ -3,6 +3,9 @@ package game;
 import interfaces.GameInterface;
 import java.awt.Graphics2D;
 import java.awt.Color;
+import util.LoadingStuffs;
+import java.awt.image.BufferedImage;
+import util.Audio;
 
 /**
  * Class representing the menu
@@ -13,20 +16,26 @@ public class MenuScreen {
     private Graphics2D g2d                  = null;
 
     //images
-    // private BufferedImage starSelected      = null;
-    
+    private BufferedImage plogo             = null;
+    private BufferedImage selector          = null;
+    private BufferedImage labelPlay         = null;
+    private BufferedImage labelOptions      = null;
+    private BufferedImage labelExit         = null;
+    private BufferedImage frame             = null;
+
     //sounds
-    // private Audio intro                     = null;
-    
+    private Audio intro                     = null;
+    private Audio item                      = null;
+    private Audio start                     = null;
+
     //menu control
     private byte selectorPosition           = 0;
-    // private byte selectors[]                = {0, 2, 3};
-    // private short optionsSpace              = 65;
-    private byte level                      = 1;
+    private byte selectors[]                = {0, 1, 2};
+    private short optionsSpace              = 90;
     private volatile boolean goOptions      = false;
     private volatile boolean goGame         = false;
     private volatile boolean goExit         = false;
-    private final Color menuColor           = new Color(0, 66, 147);
+    private final Color menuColor           = new Color(253, 187, 41);
     private int resolutionW                 = 0;
     private int resolutionH                 = 0;
 
@@ -43,11 +52,17 @@ public class MenuScreen {
         this.resolutionH        = this.gameRef.getInternalResolutionHeight();
 
         //load the images
-        // this.tlogo              = LoadingStuffs.getInstance().getImage("tlogo");
+        this.plogo              = LoadingStuffs.getInstance().getImage("popeyelogo");
+        this.selector           = LoadingStuffs.getInstance().getImage("selector");
+        this.frame              = LoadingStuffs.getInstance().getImage("menu-frame");
+        this.labelPlay          = LoadingStuffs.getInstance().getImage("lbPlayGame");
+        this.labelOptions       = LoadingStuffs.getInstance().getImage("lbOptions");
+        this.labelExit          = LoadingStuffs.getInstance().getImage("lbExitGame");
 
         //load the sounds
-        // this.intro              = LoadingStuffs.getInstance().getAudio("intro");
-        
+        this.intro              = LoadingStuffs.getInstance().getAudio("intro");
+        this.item               = LoadingStuffs.getInstance().getAudio("star");
+        this.start              = LoadingStuffs.getInstance().getAudio("start");
     }
 
     /**
@@ -55,14 +70,14 @@ public class MenuScreen {
      * @param frametime
      */
     public synchronized void update(long frametime) {
-        this.gameRef.changeGameState(StateMachine.IN_GAME);
+    
     }
 
     /**
      * Stop the menu music
      */
     public void stopMusic() {
-        // this.intro.stop();
+        this.intro.stop();
     }
 
     /**
@@ -72,6 +87,12 @@ public class MenuScreen {
     public synchronized void draw(long frametime) {
         this.g2d.setBackground(this.menuColor);
         this.g2d.clearRect(0, 0, resolutionW, resolutionH);
+        this.g2d.drawImage(this.plogo, 253, 238, null);
+        this.g2d.drawImage(this.frame, 1019, 293, null);
+        this.g2d.drawImage(this.selector, 1069, 412 + (selectors[selectorPosition] * optionsSpace), null);
+        this.g2d.drawImage(this.labelPlay, 1209, 438, null);
+        this.g2d.drawImage(this.labelOptions, 1275, 526, null);
+        this.g2d.drawImage(this.labelExit, 1256, 613, null);
     }
 
     /**
@@ -79,7 +100,7 @@ public class MenuScreen {
      * @param frametime
      */
     public synchronized void firstUpdate(long frametime) {
-        // this.intro.playContinuously();
+        this.intro.playContinuously();
     }
 
     //getters
@@ -93,15 +114,35 @@ public class MenuScreen {
      * @param key
      */
     public void keyMovement(int key) {
-        
-        if (key == 27) { //esc
 
+        if (key == 27) { //esc
+            this.goExit = true;
         } else if (key == 38) { //UP
-            
+            if (this.selectorPosition > 0) {
+                this.selectorPosition--;
+            } else {
+                this.selectorPosition = 2;
+            }
+            this.item.play();
         } else if (key == 40) { //DOWN
-            
+            if (this.selectorPosition < 2) {
+                this.selectorPosition++;
+            } else {
+                this.selectorPosition = 0;
+            }
+            this.item.play();
         } else if (key == 10) { //ENTER
-            
+            this.goExit     = false;
+            this.goOptions  = false;
+            this.goGame     = false;
+            if (this.selectorPosition == 0) {
+                this.goGame = true;
+                this.start.play();
+            } else if (this.selectorPosition == 1) {
+                this.goOptions = true;
+            } else if (this.selectorPosition == 2) {
+                this.goExit = true;
+            }
         }
     }
 
@@ -113,6 +154,4 @@ public class MenuScreen {
         this.goGame     = false;
         this.goExit     = false;
     }
-
-    public byte getLevel() {return (this.level);}
 }
