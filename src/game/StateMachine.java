d559c4f6833ca84e1bda593b1add30a7b91712ec
jpaulobf <jpaulobf@gmail.com>
2022-08-21 11:17:22 -0300

    Correct state in the State Machine

--- src/game/StateMachine.java
+++ src/game/StateMachine.java
@@ -24,7 +24,7 @@ public class StateMachine {
      * @param game
      */
     public StateMachine(Game game) {
-        this.currentState       = IN_GAME;
+        this.currentState       = MENU;
         this.referencetToGame   = game;
     }
 
